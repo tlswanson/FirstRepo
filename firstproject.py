097478275efc37ff07d82170ac1a30f9f433ed9b
tlswanson <96324050+tlswanson@users.noreply.github.com>
2021-12-17 20:04:07 -0800

    Add files via upload

---
+++ firstproject.py
@@ -0,0 +1,7 @@
+print("Let's keep track of our book genres and number of books within each genre")
+genre_n = {'historical': 82}
+genre_n['sci-fi'] = 235
+genre_n['autobiographical'] = 14
+genre_n['young adult'] = 30
+genre_n['scientific'] = 12
+print(genre_n)
