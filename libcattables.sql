0f5345cb920936b90b31454205e52573495a81ee
tcamarillo <58059849+tcamarillo@users.noreply.github.com>
2022-03-14 18:52:06 -0700

    Create libcattables.sql

---
+++ libcattables.sql
@@ -0,0 +1,31 @@
+-- libcattables
+CREATE TABLE IF NOT EXISTS Dictionaries(
+    Title,
+    Pub YYYY
+    );
+
+CREATE TABLE IF NOT EXISTS Encyclopedias(
+    Title,
+    Pub YYYY,
+    Volume,
+    Description
+    );
+
+CREATE TABLE IF NOT EXISTS StrategyGuides(
+    Game Name,
+    Guide Title
+    );
+
+CREATE TABLE IF NOT EXISTS Textbooks(
+    Title,
+    Edition,
+    Authors
+    );
+
+CREATE TABLE IF NOT EXISTS EducationalOther(
+    Title,
+
+
+
+
+-- ALTER TABLE ency RENAME COLUMN volumenum TO volume number
